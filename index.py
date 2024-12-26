bb1236dc98b255c04baa59da03dac6a0fed72db0
ArjunAgarwal101 <agarwalarjun101@gmail.com>
2024-12-26 19:59:12 +0530

    hi

---
+++ index.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("green")
+s=turtle.Screen()
+s.setup(400,300)
+turtle.title("welcome to my world")
+b=turtle.Turtle()
+for i in range(4):
+    b.forward(110)
+    b.left(90)
+turtle.mainloop()
---
+++ star.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("green")
+s=turtle.Screen()
+s.setup(400,300)
+turtle.title("welcome to my world")
+b=turtle.Turtle()
+for i in range(2):
+  b.left(120)
+  b.forward(100)
+  turtle.penup
+  turtle.goto(10,60)
+  turtle.pendown
+  turtle.forward(100)
+  for i in range(2):
+    turtle.forward(100)
+  turtle.right(120)
+turtle.mainloop()
